abea7a6b639dc6eaf6a742c29dc745c2fa68d503
vauxhall <zero.griffin@gmail.com>
2019-01-15 09:03:47 +0100

    Initial Commit

---
+++ Mac/Mac.h
@@ -0,0 +1,13 @@
+//
+//  Mac.h
+//  Mac
+//
+//  Created by vin on 15.01.19.
+//  Copyright © 2019 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Mac : NSObject
+
+@end
---
+++ Mac/Mac.m
@@ -0,0 +1,13 @@
+//
+//  Mac.m
+//  Mac
+//
+//  Created by vin on 15.01.19.
+//  Copyright © 2019 Iturbide. All rights reserved.
+//
+
+#import "Mac.h"
+
+@implementation Mac
+
+@end
