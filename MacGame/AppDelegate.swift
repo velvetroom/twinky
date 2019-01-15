437529354e579dac942268330057c1fe5e3a0522
vauxhall <zero.griffin@gmail.com>
2019-01-15 09:39:06 +0100

    Add mac game

---
+++ MacGame/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  MacGame
+//
+//  Created by vin on 15.01.19.
+//  Copyright © 2019 Iturbide. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
