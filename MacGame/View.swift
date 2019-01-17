ac474ceb4ff06ba6a66708c6e8be5ec360aa7d8a
vauxhall <zero.griffin@gmail.com>
2019-01-17 11:26:30 +0100

    Add ground tiles

--- MacGame/View.swift
+++ MacGame/View.swift
@@ -25,18 +25,17 @@ class View:NSWindow {
         twinky.position = CGPoint(x:contentView!.bounds.midX, y:contentView!.bounds.midY)
         scene.addChild(twinky)
         
-        let grassGroup = SKTileGroup(rules:[
-            SKTileGroupRule(adjacency:[], tileDefinitions:[
-                SKTileDefinition(texture:SKTexture(imageNamed:"grass-0")),
-                SKTileDefinition(texture:SKTexture(imageNamed:"grass-1")),
-                SKTileDefinition(texture:SKTexture(imageNamed:"grass-2")),
-                SKTileDefinition(texture:SKTexture(imageNamed:"grass-3")),
-                SKTileDefinition(texture:SKTexture(imageNamed:"grass-4"))])])
-        let groundGroup = SKTileGroup(rules:[
-            SKTileGroupRule(adjacency:[], tileDefinitions:[
-                SKTileDefinition(texture:SKTexture(imageNamed:"grass-0-top-0")),
-                SKTileDefinition(texture:SKTexture(imageNamed:"grass-0-top-1")),
-                SKTileDefinition(texture:SKTexture(imageNamed:"grass-0-top-2"))])])
+        var grassTiles = [SKTileDefinition]()
+        var groundTiles = [SKTileDefinition]()
+        for x in 0 ... 4 {
+            grassTiles.append(SKTileDefinition(texture:SKTexture(imageNamed:"grass-\(x)")))
+            for y in 0 ... 2 {
+                groundTiles.append(SKTileDefinition(texture:SKTexture(imageNamed:"grass-\(x)-top-\(y)")))
+            }
+        }
+        
+        let grassGroup = SKTileGroup(rules:[SKTileGroupRule(adjacency:[], tileDefinitions:grassTiles)])
+        let groundGroup = SKTileGroup(rules:[SKTileGroupRule(adjacency:[], tileDefinitions:groundTiles)])
         let grass = SKTileMapNode(tileSet:SKTileSet(tileGroups:[grassGroup]), columns:50, rows:5, tileSize:
             CGSize(width:16, height:16), fillWith:grassGroup)
         grass.position = CGPoint(x:grass.mapSize.width / 2, y:grass.mapSize.height / 2)
